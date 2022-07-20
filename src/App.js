17495b9190e03be8086610fb49a9e8615593db53
dhshffnfn123 <anbyunghoon0107@gmail.com>
2022-07-20 22:13:44 +0900

    redirect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Box from "./components/Box";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import APage from "./components/APage";
 import Bpage from "./components/Bpage";
 import ProductPage from "./components/ProductPage";
 import ProductDetailPage from "./components/ProductDetailPage";
+import LoginPage from "./components/LoginPage";
+import UserPage from "./components/UserPage";
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(true);
+
+  const PrivateRoute = () => {
+    return authenticate === true ? <UserPage /> : <Navigate to="/login" />;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
@@ -15,6 +23,8 @@ function App() {
         <Route path="/b" element={<Bpage />} />
         <Route path="/product" element={<ProductPage />} />
         <Route path="/product:id" element={<ProductDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/user" element={<PrivateRoute />} />
       </Routes>
     </BrowserRouter>
   );
