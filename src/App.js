78b25bd2db931fe8482e0c767c391066a26b2548
dhshffnfn123 <anbyunghoon0107@gmail.com>
2022-08-11 23:41:21 +0900

    로딩

--- src/App.js
+++ src/App.js
@@ -38,3 +38,6 @@ export default App;
 // private Route
 // 상태에 따른 렌더링을 할시 페이지 보호 기능.
 // useNavigator 등을 이용하여 상태에 따라 url을 설정해준다.
+
+// loading
+// api 호출 시 useState를 사용해 호출 전 true, 호출 후 false로 하여 로딩을 구현한다.
